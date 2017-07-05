c0875473db1ace6127613327d517acc7ba0b3120
kallisons <kallisonsmith@yahoo.com>
2017-07-05 15:23:44 -0700

    fix formatting of ErrorLog

--- Model/LTRANS.f90
+++ Model/LTRANS.f90
@@ -1305,7 +1305,7 @@ contains
             call setOut(n)
           endif
           OPEN(210,FILE='ErrorLog.txt',POSITION='APPEND')
-            write(210,26) n,ix(3)
+            write(210,26) n,int(ix(3))
           CLOSE(210)
           cycle
         endif
@@ -1357,11 +1357,11 @@ contains
           OPEN(210,FILE='ErrorLog.txt',POSITION='APPEND')
             SELECT CASE (ele_err)
               CASE(1,4)
-                write(210,27) n,ix(3)
+                write(210,27) n,int(ix(3))
               CASE(2,5)
-                write(210,28) n,ix(3)
+                write(210,28) n,int(ix(3))
               CASE(3,6)
-                write(210,29) n,ix(3)
+                write(210,29) n,int(ix(3))
             END SELECT
           CLOSE(210)
           cycle
